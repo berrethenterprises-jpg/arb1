f804015f1c8c3ec4fa1edb0e28e8c7dcbd12be7e
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-12 12:44:28 -0600

    Update encodeParams.js

--- utils/encodeParams.js
+++ utils/encodeParams.js
@@ -1,1 +1,8 @@
+import { ethers } from "ethers";
 
+export const encodeFlashParams = (router, swapData) => {
+    return ethers.AbiCoder.defaultAbiCoder().encode(
+        ["address", "bytes"],
+        [router, swapData]
+    );
+};
