fb54b68c0216cb248979981c737e21e1aac93c81
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-12 12:58:11 -0600

    Update crossChainSignal.js

--- utils/crossChainSignal.js
+++ utils/crossChainSignal.js
@@ -1,1 +1,12 @@
+export const getCrossChainSignal = ({
+    mainnetPrice,
+    altPrice
+}) => {
 
+    const spread = (mainnetPrice - altPrice) / altPrice;
+
+    return {
+        spread,
+        strong: Math.abs(spread) > 0.01
+    };
+};
