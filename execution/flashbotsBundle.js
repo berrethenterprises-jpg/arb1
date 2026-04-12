8ced25138d002e86b4ec465c709ae5410d575cf3
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-12 12:49:28 -0600

    Update flashbotsBundle.js

--- execution/flashbotsBundle.js
+++ execution/flashbotsBundle.js
@@ -1,1 +1,28 @@
+import { ethers } from "ethers";
+import { FlashbotsBundleProvider } from "@flashbots/ethers-provider-bundle";
 
+export const createFlashbotsProvider = async (provider, wallet) => {
+    return await FlashbotsBundleProvider.create(
+        provider,
+        wallet
+    );
+};
+
+export const sendFlashbotsBundle = async ({
+    flashbots,
+    wallet,
+    tx
+}) => {
+
+    const signed = await wallet.signTransaction(tx);
+
+    const bundle = [
+        {
+            signedTransaction: signed
+        }
+    ];
+
+    const block = await flashbots.provider.getBlockNumber();
+
+    return await flashbots.sendBundle(bundle, block + 1);
+};
