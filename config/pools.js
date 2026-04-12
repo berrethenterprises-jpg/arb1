995a219b0dacc140135f1df726e1c4f0bd9538d3
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-12 12:46:42 -0600

    Update pools.js

--- config/pools.js
+++ config/pools.js
@@ -1,1 +1,9 @@
-
+export const POOLS = {
+    ETHUSDT: [
+        "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640", // 0.05%
+        "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8", // 0.3%
+    ],
+    BTCUSDT: [
+        "0xcbcdf9626bc03e24f779434178a73a0b4bad62ed", // example
+    ]
+};
