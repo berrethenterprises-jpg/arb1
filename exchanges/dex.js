3dd38ee604489c713c452c3be64f4e967ca19083
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-12 12:06:10 -0600

    Update dex.js

--- exchanges/dex.js
+++ exchanges/dex.js
@@ -5,7 +5,6 @@ export const getDEXQuote = async (cexPrice) => {
         lastPrice = cexPrice;
     }
 
-    // 🔥 increased deviation → more opportunities
     const deviation = (Math.random() - 0.5) * 0.01;
 
     lastPrice = cexPrice * (1 + deviation);
