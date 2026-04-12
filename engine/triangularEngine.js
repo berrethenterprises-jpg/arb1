e9bb2a3048c2db76f4694eb7731762ad55b49ae3
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-12 12:50:03 -0600

    Update triangularEngine.js

--- engine/triangularEngine.js
+++ engine/triangularEngine.js
@@ -1,1 +1,15 @@
+export const findTriangular = (prices) => {
 
+    const { ETH_USDC, USDC_DAI, ETH_DAI } = prices;
+
+    if (!ETH_USDC || !USDC_DAI || !ETH_DAI) return null;
+
+    const cycle =
+        (1 / ETH_USDC) *
+        (1 / USDC_DAI) *
+        ETH_DAI;
+
+    return {
+        profitRatio: cycle
+    };
+};
