d698f0956dc2dc89ac9f16c56db14ca8952513f6
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-12 12:12:43 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 export const CONFIG = {
     PAPER_TRADING: true,
 
-    START_BALANCE: 10000,
+    START_BALANCE: 1000,   // 🔥 changed from 10000
 
     MIN_SPREAD: 0.0005,
     MIN_LIQUIDITY: 0.2,
 
-    BASE_SIZE_FACTOR: 20,   // 🔥 increased from 12
+    BASE_SIZE_FACTOR: 20,
 
     LOOP_DELAY: 200
 };
