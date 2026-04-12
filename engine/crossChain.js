0f449db7b1be2b3406b4ae6ecfbcc7c4ce1cb6c5
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-12 12:50:35 -0600

    Update crossChain.js

--- engine/crossChain.js
+++ engine/crossChain.js
@@ -1,1 +1,12 @@
+export const checkCrossChain = async ({
+    ethPrice,
+    arbPrice
+}) => {
 
+    const spread = (ethPrice - arbPrice) / arbPrice;
+
+    return {
+        spread,
+        profitable: Math.abs(spread) > 0.01 // higher threshold
+    };
+};
