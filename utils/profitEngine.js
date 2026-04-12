6f3866caf5ca80e01fec8200dbc7a7b2551953d9
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-12 12:43:37 -0600

    Update profitEngine.js

--- utils/profitEngine.js
+++ utils/profitEngine.js
@@ -1,1 +1,14 @@
+export const isProfitable = ({
+    spread,
+    gasCost,
+    tradeSize
+}) => {
 
+    const fee = 0.002; // 0.2%
+
+    const gross = tradeSize * spread;
+
+    const net = gross - (tradeSize * fee) - gasCost;
+
+    return net > 0;
+};
