ff9acc8911899d86eaa2280614b13aa51484101e
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-12 12:57:40 -0600

    Update multiHop.js

--- utils/multiHop.js
+++ utils/multiHop.js
@@ -1,1 +1,20 @@
+export const simulateMultiHop = ({
+    amount,
+    path,
+    prices
+}) => {
 
+    let current = amount;
+
+    for (let i = 0; i < path.length - 1; i++) {
+
+        const pair = `${path[i]}_${path[i + 1]}`;
+        const price = prices[pair];
+
+        if (!price) return null;
+
+        current *= price;
+    }
+
+    return current;
+};
