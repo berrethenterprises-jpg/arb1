a9d6e9e355346065f2941c5f982780e693ccd09e
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-12 18:19:10 -0600

    Update sushiswap.js

--- dex/sushiswap.js
+++ dex/sushiswap.js
@@ -1,6 +1,5 @@
 import { ethers } from "ethers";
 
-// Sushi ETH/USDC pool
 const PAIR = "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0";
 
 const ABI = [
@@ -9,15 +8,13 @@ const ABI = [
 
 export const getSushiPrice = async (provider) => {
   try {
-    const contract = new ethers.Contract(PAIR, ABI, provider);
-    const [r0, r1] = await contract.getReserves();
+    const c = new ethers.Contract(PAIR, ABI, provider);
+    const [r0, r1] = await c.getReserves();
 
     const usdc = parseFloat(ethers.utils.formatUnits(r0, 6));
     const eth = parseFloat(ethers.utils.formatUnits(r1, 18));
 
-    if (!eth) return null;
-
-    return usdc / eth;
+    return eth ? usdc / eth : null;
   } catch {
     return null;
   }
