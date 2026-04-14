a2ad9fdae014409d01fa3c58a585c6dec30626e6
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-13 19:50:58 -0600

    Update uniswap.js

--- dex/uniswap.js
+++ dex/uniswap.js
@@ -1,18 +1,15 @@
 import { ethers } from "ethers";
 
 const PAIRS = [
-  // ETH/USDC
-  "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc",
-
-  // ETH/USDT
-  "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852",
-
-  // ETH/DAI
-  "0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11"
+  "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc", // ETH/USDC
+  "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852", // ETH/USDT
+  "0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11"  // ETH/DAI
 ];
 
 const ABI = [
-  "function getReserves() view returns (uint112,uint112,uint32)"
+  "function getReserves() view returns (uint112,uint112,uint32)",
+  "function token0() view returns (address)",
+  "function token1() view returns (address)"
 ];
 
 export const getUniswapPools = async (provider) => {
@@ -21,13 +18,18 @@ export const getUniswapPools = async (provider) => {
   for (const address of PAIRS) {
     try {
       const c = new ethers.Contract(address, ABI, provider);
+
       const [r0, r1] = await c.getReserves();
+      const token0 = (await c.token0()).toLowerCase();
+      const token1 = (await c.token1()).toLowerCase();
 
       results.push({
         dex: "UNI",
         address,
-        reserve0: parseFloat(ethers.utils.formatUnits(r0, 18)),
-        reserve1: parseFloat(ethers.utils.formatUnits(r1, 18))
+        token0,
+        token1,
+        reserve0: Number(ethers.utils.formatUnits(r0, 18)),
+        reserve1: Number(ethers.utils.formatUnits(r1, 18))
       });
 
     } catch {}
