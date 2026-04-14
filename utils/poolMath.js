7b7c6700aac00c37a3e578c382416b9573685ff2
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-13 20:24:17 -0600

    Update poolMath.js

--- utils/poolMath.js
+++ utils/poolMath.js
@@ -1,5 +1,10 @@
 export const getAmountOut = (amountIn, reserveIn, reserveOut) => {
+  if (!amountIn || !reserveIn || !reserveOut) return null;
+
+  // Uniswap v2 formula with fee
   const amountInWithFee = amountIn * 997;
-  return (amountInWithFee * reserveOut) /
-    (reserveIn * 1000 + amountInWithFee);
+  const numerator = amountInWithFee * reserveOut;
+  const denominator = reserveIn * 1000 + amountInWithFee;
+
+  return numerator / denominator;
 };
