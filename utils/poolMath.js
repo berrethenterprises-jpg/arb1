8863a9c6c20493288932b032badf6e584a77a195
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-13 18:46:42 -0600

    Create poolMath.js

---
+++ utils/poolMath.js
@@ -0,0 +1,6 @@
+export const getAmountOut = (amountIn, reserveIn, reserveOut) => {
+  const amountInWithFee = amountIn * 997;
+  const numerator = amountInWithFee * reserveOut;
+  const denominator = reserveIn * 1000 + amountInWithFee;
+  return numerator / denominator;
+};
