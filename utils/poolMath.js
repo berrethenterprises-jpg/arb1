9b6cf109e352e1ab7bd2970088258bec05b204cf
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-13 19:08:45 -0600

    Update poolMath.js

--- utils/poolMath.js
+++ utils/poolMath.js
@@ -1,11 +1,5 @@
 export const getAmountOut = (amountIn, reserveIn, reserveOut) => {
   const amountInWithFee = amountIn * 997;
-  const numerator = amountInWithFee * reserveOut;
-  const denominator = reserveIn * 1000 + amountInWithFee;
-  return numerator / denominator;
-};
-
-// 🔥 NEW: reverse swap (USDC → ETH)
-export const getAmountOutReverse = (amountIn, reserveUSDC, reserveETH) => {
-  return getAmountOut(amountIn, reserveUSDC, reserveETH);
+  return (amountInWithFee * reserveOut) /
+    (reserveIn * 1000 + amountInWithFee);
 };
