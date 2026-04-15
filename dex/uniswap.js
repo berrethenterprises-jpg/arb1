c14d2b67942604a0eb0be8ebfbc97916c8ce9d36
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-14 20:47:53 -0600

    Update uniswap.js

--- dex/uniswap.js
+++ dex/uniswap.js
@@ -1,5 +1,4 @@
 import { ethers } from "ethers";
-import { POOL_LIST } from "./poolList.js";
 
 const ABI = [
   "function getReserves() view returns (uint112,uint112,uint32)",
@@ -7,8 +6,19 @@ const ABI = [
   "function token1() view returns (address)"
 ];
 
+// 🔥 CONNECTED + LIQUID POOLS
+const PAIRS = [
+  "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc", // USDC/WETH
+  "0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11", // DAI/WETH
+  "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852", // USDT/WETH
+  "0xBb2b8038a1640196FbE3e38816F3e67Cba72D940", // WBTC/WETH
+  "0x3041CbD36888bECc7bbCBc0045E3B1f144466f5f", // USDC/USDT
+  "0xAE461cA67B15dc8d2dA7c1dD4A90c3c6C5E7eF9A", // USDC/DAI
+  "0x5777d92f208679DB4b9778590Fa3CAB3aC9e2168"
+];
+
 export const getUniswapPools = async (provider) => {
-  const calls = POOL_LIST.map(async (addr) => {
+  const calls = PAIRS.map(async (addr) => {
     try {
       const c = new ethers.Contract(addr, ABI, provider);
 
