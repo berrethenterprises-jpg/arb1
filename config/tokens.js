6e156ab6e7cfe6a57384a0979d599e8a28623e26
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-04-19 13:47:33 -0600

    Create tokens.js

---
+++ config/tokens.js
@@ -0,0 +1,19 @@
+export const TOKENS = [
+  // Core
+  { symbol: "WETH", address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", decimals: 18 },
+  { symbol: "USDC", address: "0xA0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", decimals: 6 },
+  { symbol: "USDT", address: "0xdAC17F958D2ee523a2206206994597C13D831ec7", decimals: 6 },
+  { symbol: "DAI",  address: "0x6B175474E89094C44Da98b954EedeAC495271d0F", decimals: 18 },
+
+  // High liquidity alts
+  { symbol: "WBTC", address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599", decimals: 8 },
+  { symbol: "LINK", address: "0x514910771AF9Ca656af840dff83E8264EcF986CA", decimals: 18 },
+  { symbol: "UNI",  address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984", decimals: 18 },
+  { symbol: "AAVE", address: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", decimals: 18 },
+  { symbol: "MKR",  address: "0x9f8F72aA9304c8B593d555F12ef6589cC3A579A2", decimals: 18 },
+  { symbol: "SNX",  address: "0xC011A72400E58ecD99Ee497CF89E3775d4bd732F", decimals: 18 },
+
+  // Extras
+  { symbol: "CRV",  address: "0xD533a949740bb3306d119CC777fa900bA034cd52", decimals: 18 },
+  { symbol: "COMP", address: "0xc00e94Cb662C3520282E6f5717214004A7f26888", decimals: 18 }
+];
